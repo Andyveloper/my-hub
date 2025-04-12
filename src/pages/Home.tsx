772d296c94951e853e132bd7ddc8f4e0f5e7eed6
Andres Mauricio Cantillo <96924302+Andyveloper@users.noreply.github.com>
2025-04-12 14:52:23 -0500

    Reemplazar GUILTY_GEAR_replay.mp4 por v2 en Home, actualiza archivos y usa Git LFS.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import GUILTY_GEAR_replay from '@src/assets/videos/GUILTY_GEAR_replay.mp4'
+import GUILTY_GEAR_replay_v2 from '@src/assets/videos/GUILTY_GEAR_replay_v2.mp4'
 import VideoHeader from '@src/pages/pageComponents/general/VideoHeader'
 import ShowList from '@src/pages/pageComponents/home/ShowList'
 import AboutMe from '@src/pages/pageComponents/home/AboutMe'
@@ -18,7 +18,7 @@ import {
 const Home = () => {
   return (
     <main>
-      <VideoHeader video={GUILTY_GEAR_replay} title={headerTitle} text={headerText} />
+      <VideoHeader video={GUILTY_GEAR_replay_v2} title={headerTitle} text={headerText} />
       <AboutMe title={aboutMeTitle} subTitle={aboutMeSubtitle} description={aboutMeDescription} />
       <ShowList showList={appShowList} title={showListTitle} description={showListDescription} />
     </main>
