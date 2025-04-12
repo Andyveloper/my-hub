53e9182641565495dc4b368be73bd215cdbad6c1
Andres Mauricio Cantillo <96924302+Andyveloper@users.noreply.github.com>
2025-04-12 17:17:33 -0500

    Añadir componentes Navbar y Footer al render principal de la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import './index.css'
 import router from './routes/Routes'
+import Footer from './pages/pageComponents/general/Footer'
+import Navbar from './pages/pageComponents/general/Navbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Navbar />
     <RouterProvider router={router} />
+    <Footer />
   </StrictMode>
 )
